a016db7a241030e15291cd6483b1f8623fda8f4a
[domin7] <[aidominguez@outlook.es]>
2023-04-27 16:25:15 +0200

    completadas las tablas

--- src/Entity/Interaction.php
+++ src/Entity/Interaction.php
@@ -14,11 +14,19 @@ class Interaction
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column]
+    #[ORM\Column(nullable: true)]
     private ?bool $user_favorite = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $comment = null;
+
+    #[ORM\ManyToOne(inversedBy: 'interactions')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    #[ORM\ManyToOne(inversedBy: 'interactions')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Post $post = null;
 
     public function getId(): ?int
     {
@@ -45,6 +53,30 @@ class Interaction
     public function setComment(?string $comment): self
     {
         $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getPost(): ?Post
+    {
+        return $this->post;
+    }
+
+    public function setPost(?Post $post): self
+    {
+        $this->post = $post;
 
         return $this;
     }
